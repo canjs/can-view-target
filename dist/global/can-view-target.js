f5067305092a7fe82fe9c122d52370e707056139
David Luecke <daff@neyeon.com>
2016-04-25 13:39:34 -0700

    3.0.0-pre.1

---
+++ dist/global/can-view-target.js
@@ -0,0 +1,254 @@
+/*[global-shim-start]*/
+(function(exports, global, doEval){ // jshint ignore:line
+	var origDefine = global.define;
+
+	var get = function(name){
+		var parts = name.split("."),
+			cur = global,
+			i;
+		for(i = 0 ; i < parts.length; i++){
+			if(!cur) {
+				break;
+			}
+			cur = cur[parts[i]];
+		}
+		return cur;
+	};
+	var modules = (global.define && global.define.modules) ||
+		(global._define && global._define.modules) || {};
+	var ourDefine = global.define = function(moduleName, deps, callback){
+		var module;
+		if(typeof deps === "function") {
+			callback = deps;
+			deps = [];
+		}
+		var args = [],
+			i;
+		for(i =0; i < deps.length; i++) {
+			args.push( exports[deps[i]] ? get(exports[deps[i]]) : ( modules[deps[i]] || get(deps[i]) )  );
+		}
+		// CJS has no dependencies but 3 callback arguments
+		if(!deps.length && callback.length) {
+			module = { exports: {} };
+			var require = function(name) {
+				return exports[name] ? get(exports[name]) : modules[name];
+			};
+			args.push(require, module.exports, module);
+		}
+		// Babel uses the exports and module object.
+		else if(!args[0] && deps[0] === "exports") {
+			module = { exports: {} };
+			args[0] = module.exports;
+			if(deps[1] === "module") {
+				args[1] = module;
+			}
+		} else if(!args[0] && deps[0] === "module") {
+			args[0] = { id: moduleName };
+		}
+
+		global.define = origDefine;
+		var result = callback ? callback.apply(null, args) : undefined;
+		global.define = ourDefine;
+
+		// Favor CJS module.exports over the return value
+		modules[moduleName] = module && module.exports ? module.exports : result;
+	};
+	global.define.orig = origDefine;
+	global.define.modules = modules;
+	global.define.amd = true;
+	ourDefine("@loader", [], function(){
+		// shim for @@global-helpers
+		var noop = function(){};
+		return {
+			get: function(){
+				return { prepareGlobal: noop, retrieveGlobal: noop };
+			},
+			global: global,
+			__exec: function(__load){
+				doEval(__load.source, global);
+			}
+		};
+	});
+}
+)({},window,function(__$source__, __$global__) { // jshint ignore:line
+	eval("(function() { " + __$source__ + " \n }).call(__$global__);");
+}
+)
+/*can-view-target@3.0.0-pre.0#can-view-target*/
+define('can-view-target', function (require, exports, module) {
+    var childNodes = require('can-util/dom/child-nodes/child-nodes');
+    var domAttr = require('can-util/dom/attr/attr');
+    var each = require('can-util/js/each/each');
+    var makeArray = require('can-util/js/make-array/make-array');
+    var getDocument = require('can-util/dom/document/document');
+    var domMutate = require('can-util/dom/mutate/mutate');
+    var processNodes = function (nodes, paths, location, document) {
+            var frag = document.createDocumentFragment();
+            for (var i = 0, len = nodes.length; i < len; i++) {
+                var node = nodes[i];
+                frag.appendChild(processNode(node, paths, location.concat(i), document));
+            }
+            return frag;
+        }, keepsTextNodes = typeof document !== 'undefined' && function () {
+            var testFrag = document.createDocumentFragment();
+            var div = document.createElement('div');
+            div.appendChild(document.createTextNode(''));
+            div.appendChild(document.createTextNode(''));
+            testFrag.appendChild(div);
+            var cloned = testFrag.cloneNode(true);
+            return childNodes(cloned.firstChild).length === 2;
+        }(), clonesWork = typeof document !== 'undefined' && function () {
+            var a = document.createElement('a');
+            a.innerHTML = '<xyz></xyz>';
+            var clone = a.cloneNode(true);
+            return clone.innerHTML === '<xyz></xyz>';
+        }(), namespacesWork = typeof document !== 'undefined' && !!document.createElementNS;
+    var cloneNode = clonesWork ? function (el) {
+        return el.cloneNode(true);
+    } : function (node) {
+        var copy;
+        if (node.nodeType === 1) {
+            copy = document.createElement(node.nodeName);
+        } else if (node.nodeType === 3) {
+            copy = document.createTextNode(node.nodeValue);
+        } else if (node.nodeType === 8) {
+            copy = document.createComment(node.nodeValue);
+        } else if (node.nodeType === 11) {
+            copy = document.createDocumentFragment();
+        }
+        if (node.attributes) {
+            var attributes = makeArray(node.attributes);
+            each(attributes, function (node) {
+                if (node && node.specified) {
+                    domAttr.setAttribute(copy, node.nodeName, node.nodeValue);
+                }
+            });
+        }
+        if (node.childNodes) {
+            each(node.childNodes, function (child) {
+                copy.appendChild(cloneNode(child));
+            });
+        }
+        return copy;
+    };
+    function processNode(node, paths, location, document) {
+        var callback, loc = location, nodeType = typeof node, el, p, i, len;
+        var getCallback = function () {
+            if (!callback) {
+                callback = {
+                    path: location,
+                    callbacks: []
+                };
+                paths.push(callback);
+                loc = [];
+            }
+            return callback;
+        };
+        if (nodeType === 'object') {
+            if (node.tag) {
+                if (namespacesWork && node.namespace) {
+                    el = document.createElementNS(node.namespace, node.tag);
+                } else {
+                    el = document.createElement(node.tag);
+                }
+                if (node.attrs) {
+                    for (var attrName in node.attrs) {
+                        var value = node.attrs[attrName];
+                        if (typeof value === 'function') {
+                            getCallback().callbacks.push({ callback: value });
+                        } else {
+                            domAttr.setAttribute(el, attrName, value);
+                        }
+                    }
+                }
+                if (node.attributes) {
+                    for (i = 0, len = node.attributes.length; i < len; i++) {
+                        getCallback().callbacks.push({ callback: node.attributes[i] });
+                    }
+                }
+                if (node.children && node.children.length) {
+                    if (callback) {
+                        p = callback.paths = [];
+                    } else {
+                        p = paths;
+                    }
+                    el.appendChild(processNodes(node.children, p, loc, document));
+                }
+            } else if (node.comment) {
+                el = document.createComment(node.comment);
+                if (node.callbacks) {
+                    for (i = 0, len = node.attributes.length; i < len; i++) {
+                        getCallback().callbacks.push({ callback: node.callbacks[i] });
+                    }
+                }
+            }
+        } else if (nodeType === 'string') {
+            el = document.createTextNode(node);
+        } else if (nodeType === 'function') {
+            if (keepsTextNodes) {
+                el = document.createTextNode('');
+                getCallback().callbacks.push({ callback: node });
+            } else {
+                el = document.createComment('~');
+                getCallback().callbacks.push({
+                    callback: function () {
+                        var el = document.createTextNode('');
+                        domMutate.replaceChild.call(this.parentNode, el, this);
+                        return node.apply(el, arguments);
+                    }
+                });
+            }
+        }
+        return el;
+    }
+    function getCallbacks(el, pathData, elementCallbacks) {
+        var path = pathData.path, callbacks = pathData.callbacks, paths = pathData.paths, child = el, pathLength = path ? path.length : 0, pathsLength = paths ? paths.length : 0;
+        for (var i = 0; i < pathLength; i++) {
+            child = child.childNodes.item(path[i]);
+        }
+        for (i = 0; i < pathsLength; i++) {
+            getCallbacks(child, paths[i], elementCallbacks);
+        }
+        elementCallbacks.push({
+            element: child,
+            callbacks: callbacks
+        });
+    }
+    function hydrateCallbacks(callbacks, args) {
+        var len = callbacks.length, callbacksLength, callbackElement, callbackData;
+        for (var i = 0; i < len; i++) {
+            callbackData = callbacks[i];
+            callbacksLength = callbackData.callbacks.length;
+            callbackElement = callbackData.element;
+            for (var c = 0; c < callbacksLength; c++) {
+                callbackData.callbacks[c].callback.apply(callbackElement, args);
+            }
+        }
+    }
+    function makeTarget(nodes, doc) {
+        var paths = [];
+        var frag = processNodes(nodes, paths, [], doc || getDocument());
+        return {
+            paths: paths,
+            clone: frag,
+            hydrate: function () {
+                var cloned = cloneNode(this.clone);
+                var args = makeArray(arguments);
+                var callbacks = [];
+                for (var i = 0; i < paths.length; i++) {
+                    getCallbacks(cloned, paths[i], callbacks);
+                }
+                hydrateCallbacks(callbacks, args);
+                return cloned;
+            }
+        };
+    }
+    makeTarget.keepsTextNodes = keepsTextNodes;
+    module.exports = makeTarget;
+});
+/*[global-shim-end]*/
+(function(){ // jshint ignore:line
+	window._define = window.define;
+	window.define = window.define.orig;
+}
+)();
